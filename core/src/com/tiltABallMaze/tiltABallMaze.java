83ca487e0a2a93e04fed5d027432b483c7d00879
Mark Kuczmarski <kuczmama@miamiOH.edu>
2015-01-05 20:49:12 +0100

    got accelerometer physics working the image will move with the phone's location also there is error checking for the edges of the phone

--- core/src/com/tiltABallMaze/tiltABallMaze.java
+++ core/src/com/tiltABallMaze/tiltABallMaze.java
@@ -9,19 +9,44 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class tiltABallMaze extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private float ballX = 0.0f;
+	private float ballY = 0.0f;
+	private float screenWidth;
+	private float screenHeight;
+	private final float speedConstant = 1.5f;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
 		img = new Texture("badlogic.jpg");
+	}
+
+	//test the physics
+	private void calculateImageLocation(){		
+		ballX += Gdx.input.getAccelerometerY() * speedConstant;
+		ballY += -Gdx.input.getAccelerometerX() * speedConstant;
+		if(ballX <= 0){
+			ballX = 0;
+		} else if(ballX >= (screenWidth - img.getWidth())){
+			ballX = screenWidth - img.getWidth();
+		}
+		if(ballY <= 0 ){
+			ballY = 0;
+		} else if(ballY >= (screenHeight - img.getHeight())){
+			ballY = screenHeight - img.getHeight();
+		}
+		
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		calculateImageLocation();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, ballX, ballY);
 		batch.end();
 	}
 }
