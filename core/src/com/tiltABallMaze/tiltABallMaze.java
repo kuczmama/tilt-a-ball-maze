c6ab28e1408b79189ceac6798a4897ff6039ef16
Mark Kuczmarski <kuczmama@miamiOH.edu>
2015-01-08 19:46:47 +0100

    Figured out the how to get the correct pixel, it's just YHeight - height, now I need to get the collisions working

--- core/src/com/tiltABallMaze/tiltABallMaze.java
+++ core/src/com/tiltABallMaze/tiltABallMaze.java
@@ -19,8 +19,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class tiltABallMaze extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	private float ballX = 200.0f;
-	private float ballY = 100.0f;
+	private float ballX = 50.0f;
+	private float ballY = 50.0f;
 	private float screenWidth;
 	private float screenHeight;
 	private final float speedConstant = 0.5f;
@@ -32,7 +32,8 @@ public class tiltABallMaze extends ApplicationAdapter {
 	private boolean desktop = false;
 	private BitmapFont font;
 	private Pixmap pixmap;
-	
+	private static Texture test;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -46,12 +47,12 @@ public class tiltABallMaze extends ApplicationAdapter {
 		font.setColor(Color.RED);
 		convertPixmap(level1);
 		//pixmap = createPro
-		
+
 	}
 
 	public static void load(){
 		level1 = loadPixmap("levels/testlevel.bmp");
-		level1tex = new Texture(level1);
+		test = new Texture(level1);
 		//level1tex = loadTexture("levels/testlevel.bmp");
 	}
 
@@ -72,67 +73,54 @@ public class tiltABallMaze extends ApplicationAdapter {
 		G = ((value & 0x00ff0000) >>> 16);
 		B = ((value & 0x0000ff00) >>> 8);
 		A = ((value & 0x000000ff));
-		
+
 		return R <= 255 && R > min  && G <= 255 && G > min && B <= 255 && B > min && A <= 255 && A > min;
 	}
 
 
 	//test the physics
 	private void calculateImageLocation(){	
-		
-		/*if(desktop){
-			if(Gdx.input.isKeyJustPressed(Input.Keys.W)){
-				ballY++;
+		float tmpX = ballX;
+		float tmpY = ballY;
+		float deltaY = -Gdx.input.getAccelerometerX() * speedConstant;
+		float deltaX = Gdx.input.getAccelerometerY() * speedConstant;
+		int value = level1.getPixel((int)ballX,(int)screenHeight - (int)ballY);
+		R = ((value & 0xff000000) >>> 24);
+		G = ((value & 0x00ff0000) >>> 16);
+		B = ((value & 0x0000ff00) >>> 8);
+		A = ((value & 0x000000ff));
+		ballX += deltaY;
+		ballY += deltaX;
+		//int pixel = level1.getPixel((int)ballX,(int)ballY);
+		if(isWhite(value)){
+			ballX += deltaX;
+			ballY += deltaY;
+		}
+		if(!isWhite(level1.getPixel((int)ballX + (int)deltaX,(int)screenHeight - (int)ballY + (int)deltaY))) {
+			ballX = tmpX;
+			ballY = tmpY;
+			if(ballX <= 0){
+				ballX = 0;
+			} else if(ballX >= (screenWidth - boxSize)){
+				ballX = screenWidth - boxSize;
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.S)){
-				ballY--;
+			if(ballY <= 0 ){
+				ballY = 0;
+			} else if(ballY >= (screenHeight - boxSize)){
+				ballY = screenHeight - boxSize;
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.A)){
-				ballX--;
-			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.D)){
-				ballX++;
-			}
-
-		} else {*/
-			//float tmpX = ballX;
-			//float tmpY = ballY;
-			float deltaY = -Gdx.input.getAccelerometerX() * speedConstant;
-			float deltaX = Gdx.input.getAccelerometerY() * speedConstant;
-			int value = level1.getPixel((int)ballX,(int)ballY);
-			R = ((value & 0xff000000) >>> 24);
-			G = ((value & 0x00ff0000) >>> 16);
-			B = ((value & 0x0000ff00) >>> 8);
-			A = ((value & 0x000000ff));
-			//ballX += deltaY;
-			//ballY += deltaX;
-			//int pixel = level1.getPixel((int)ballX,(int)ballY);
-			//if(isWhite(level1.getPixel((int)(ballX + deltaY),(int)(ballX + deltaX)))){
-				ballX += deltaX;
-				ballY += deltaY;
-			//}
-			//if(!isWhite(level1.getPixel((int)(ballX + deltaY),(int)(ballX + deltaX)))) {
-				//ballX = tmpX;
-				//ballY = tmpY;
-				if(ballX <= 0){
-					ballX = 0;
-				} else if(ballX >= (screenWidth - boxSize)){
-					ballX = screenWidth - boxSize;
-				}
-				if(ballY <= 0 ){
-					ballY = 0;
-				} else if(ballY >= (screenHeight - boxSize)){
-					ballY = screenHeight - boxSize;
-				}
-			//} 
-		//}
+		}
 	}
-	
-	private void convertPixmap(Pixmap pixmap){
+
+	private void convertPixmap(Pixmap original){
 		Pixmap tmp = new Pixmap((int)screenWidth,(int)screenHeight,Format.RGBA8888);
-		tmp.drawPixmap(level1, 0, 0);
+		//tmp.drawPixmap(level1,0,0);
+		tmp.drawPixmap(level1, 0, 0,0,0,(int)screenWidth,(int)screenHeight);
 		level1.dispose();
 		level1 = tmp;
+		//level1tex = new Texture(level1);
+		level1tex = new Texture(level1);
+		level1tex.draw(level1, 0, 0);
 	}
 
 
@@ -143,14 +131,12 @@ public class tiltABallMaze extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		calculateImageLocation();
 		batch.begin();
-		batch.draw(level1tex,screenWidth,screenHeight);
-		//batch.draw(level1tex,0,0);
-		//batch.draw(level1tex,0,0,screenWidth,screenHeight);
-		//level1.drawPixmap(pixmap, 0, 0, (int)screenWidth, (int)screenHeight, A, A);
+		batch.draw(level1tex,0,0);
+		//level1tex.draw(level1,0,0);
 		batch.draw(img, ballX, ballY,boxSize,boxSize);
-		
-		font.draw(batch, R + " " + B + " " + G + " " + A + " " + (int)ballX + " " + (int)ballY + " " + level1.getWidth() + " " + screenWidth, 50, 50);
-		
+
+		font.draw(batch, R + " " + B + " " + G + " " + A + " " + (int)ballX + " " + (int)ballY, 50, 50);
+
 		batch.end();
 	}
 }
