d1159675e868f6aae25f24268d89c9b267642c40
Mark Kuczmarski <kuczmama@miamiOH.edu>
2015-02-05 16:56:29 -0500

    Got the scrollPane to work well and also added a play button to the main menu

--- core/src/com/TWINcoGames/tiltABallMaze/Screens/MainScreen.java
+++ core/src/com/TWINcoGames/tiltABallMaze/Screens/MainScreen.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 
 import com.TWINcoGames.Helpers.Assets;
 import com.TWINcoGames.tiltABallMaze.tiltABallMaze;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
+
 
 public class MainScreen extends AbstractScreen{
 	Rectangle[] levelButtons;
@@ -17,61 +23,39 @@ public class MainScreen extends AbstractScreen{
 	ArrayList<String> levels;
 	private Table table;
 	private ScrollPane scrollPane;
-	
+	private TextButton playButton;
+
 	public MainScreen(tiltABallMaze game) {
 		this.game = game;
 		table = new Table(skin);
+		playButton = new TextButton("Play", skin);
 	}
-	
+
+	/**
+	 * Show all of the levels to the screen in a scrollable list
+	 */
 	@Override
 	public void show() {
-		List<String[]> list = new List<String[]>(skin);
+		final List<String[]> list = new List<String[]>(skin);
 		Array<String> data = new Array<String>();
 		table.setFillParent(true);
-		for(int i = 0; i < Assets.levels.size(); i++){
+		//show unlocked levels to the screen
+		for(int i = 0; i < settings.getHighestUnlockedLevel() && i < Assets.levels.size(); i++){
 			data.add("level " + Integer.toString(i + 1));
 		}
 		list.setItems(data);
-		scrollPane = new ScrollPane(list, skin);
-		table.add(scrollPane);
-		stage.addActor(table);
-	}
-	/**
-	 * Show levels in a list on the screen
-	 * with rectangle bounds so that they can be clicked
-	 * on and selected
-	 */
-	private void showLevelsList(){
-		List<String[]> list = new List<String[]>(skin);
-		Array<String> data = new Array<String>();
-		list.setItems(data);
-		table.setFillParent(true);
-		for(int i = 10; i >= 0; i--){
-			data.add("level " + Integer.toString(i + 1));
-		}
-		scrollPane = new ScrollPane(list, skin);
-		table.add(scrollPane);
-		stage.addActor(table);
-	}
-
-	@Override
-	public void render(float delta) {
-		super.render(delta);
-		showLevelsList();
-		processLevelClicks();
-	}
-
-	/**
-	 * Checks to see if a certain level is clicked, if it is
-	 * change the view to that level
-	 */
-	private void processLevelClicks() {
-		/*for(int i = 0; i < unlockedLevelButtons.length; i++){
-			//drawShape.drawRectangle(unlockedLevelButtons[i]);
-			if(screenHelper.isTouching(unlockedLevelButtons[i])){
-				game.setScreen(new LevelScreen(Assets.levels.get(Assets.levels.size() - i - 1),game));
+		//Go to the level when desired
+		playButton.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game)Gdx.app.getApplicationListener()).setScreen(
+						new LevelScreen(Assets.levels.get(list.getSelectedIndex()),game));
 			}
-		}*/
+		});
+		//format the table
+		scrollPane = new ScrollPane(list, skin);
+		table.add(scrollPane).expandX().left().expandY();
+		table.add(playButton).left().expandX();
+		stage.addActor(table);
 	}
-	
 }
